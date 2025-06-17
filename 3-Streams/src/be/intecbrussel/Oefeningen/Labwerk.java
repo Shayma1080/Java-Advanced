ed01cd33547669f359638610b07c8c20128bc8b1
Shayma1080 <hajji.chaima@outlook.fr>
2025-06-17 20:39:39 +0200

    update

--- 3-Streams/src/be/intecbrussel/Oefeningen/Labwerk.java
+++ 3-Streams/src/be/intecbrussel/Oefeningen/Labwerk.java
@@ -2,6 +2,7 @@ package be.intecbrussel.Oefeningen;
 
 import java.sql.SQLOutput;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,21 +16,83 @@ public class Labwerk {
     //Oefening 2:
     public List<String> filterA(List<String> strings2){
 
-        System.out.println(strings2.stream().filter(string ->string.contains("a")).collect(Collectors.toList()));
+        System.out.println(strings2.stream().filter(string ->string.startsWith("a")).collect(Collectors.toList()));
         return strings2;
     }
 
     //Oefening 3:
     public List<String> filterB(List<String> strings3){
-        System.out.println(strings3.stream().filter(e -> e.contains("a") && e.length() > 3).collect(Collectors.toList()));
+        System.out.println(strings3.stream().filter(e -> e.startsWith("a") && e.length() > 3).collect(Collectors.toList()));
         return strings3;
     }
 
     //Oefening 4:
     public List<String> filterC(List<String> strings4){
-        strings4.stream().filter(e -> e.startsWith("a")).map(e -> e.length()>3).limit(5).sorted(p1 -> p1,p2 -> p2.Compareto.P1).forEach(e -> System.out.println(e));
+        List<String> filterend = strings4.stream()
+                .filter(e -> e.startsWith("a") && e.length()>3)
+                .sorted((p1,p2) -> p2.compareTo(p1)) //eerste manier
+                //.sorted(Comparator.reverseOrder()) // tweede manier
+                .collect(Collectors.toList());
+        System.out.println(filterend);
 
-        return reverse;
+        return filterend;
+    }
+    //Oefening 5:
+    public List<String> filterD(List<String> strings5){
+        String n = strings5.stream()
+                .filter(e -> e.startsWith("a") && e.length()>3)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(" ,"));
+        System.out.println(n);
+        return strings5;
+    }
+    //Oefening 6:
+    public List<String> filterE(List<String> strings6){
+        String n = strings6.stream()
+                .filter(e -> e.startsWith("a") && e.length()>3)
+                .map(e -> e.toUpperCase())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(" ,"));
+        System.out.println(n);
+        return strings6;
+    }
+    //Oefening 7:
+    public List<Integer> gemiddelde(List<Integer> intgers1){
+        int even = intgers1.stream()
+                .filter(i -> i %2 == 0)
+                .reduce(0,(a,b) -> a+b);
+        System.out.println(even);
+        return intgers1;
+    }
+    //Oefening 8:
+    public List<String> lengte(List<String> strings7){
+        String myString = strings7.stream()
+                .reduce("",(s1,s2) ->s1.length() >= s2.length()? s1: s2);
+        System.out.println(myString);
+        return strings7;
+    }
+    //Oefening 9:
+    public List<Integer> evenGetallen(List<Integer> intgers2){
+         intgers2.stream()
+                .filter(i -> i %2 == 0)
+                 .forEach(System.out::println);
+        return intgers2;
+    }
+    //Oefening 10:
+    public List<String> vindenWoord(List<String> strings8){
+        strings8.stream()
+                .filter(e -> e.startsWith("aap"))
+                .forEach((e ) -> System.out.println(e));
+
+        return  strings8;
+    }
+    //Oefening 10:
+    public List<Integer> geenDouble(List<Integer> integers3){
+        integers3.stream()
+                .distinct() // verwijdert dubbele elementen
+                .forEach(System.out::println);
+
+        return  integers3;
     }
 
 }
--- 3-Streams/src/be/intecbrussel/Oefeningen/LabwerkApp.java
+++ 3-Streams/src/be/intecbrussel/Oefeningen/LabwerkApp.java
@@ -8,16 +8,48 @@ public class LabwerkApp {
         Labwerk labwerk = new Labwerk();
 
         //Oefening 1:
-        List<String> mystring1 = Arrays.asList("hello", "intec", "brussel");
+        System.out.println("Oefening 1: ");
+        List<String> mystring1 = Arrays.asList("basilic", "world", "ardennen", "is", "daar","appel","aap");
         labwerk.toUppercase(mystring1);
 
         //Oefening 2:
-        List<String> mystring2 = Arrays.asList("Basilic", "World", "waar", "is", "daar");
-        labwerk.filterA(mystring2);
+        System.out.println("\nOefening 2: ");
+        labwerk.filterA(mystring1);
         //Oefening 3:
-        labwerk.filterB(mystring2);
+        System.out.println("\nOefening 3: ");
+        labwerk.filterB(mystring1);
 
         //Oefening 4:
-        labwerk.filterC(mystring2);
+        System.out.println("\nOefening 4: ");
+        labwerk.filterC(mystring1);
+
+        //Oefening 5:
+        System.out.println("\nOefening 5: ");
+        labwerk.filterD(mystring1);
+
+        //Oefening 6:
+        System.out.println("\nOefening 6: ");
+        labwerk.filterE(mystring1);
+
+        //Oefening 7:
+        System.out.println("\nOefening 7: ");
+        List<Integer> numbers = Arrays.asList(6,7,3,22,5,3);
+        labwerk.gemiddelde(numbers);
+
+        //Oefening 8:
+        System.out.println("\nOefening 8: ");
+        labwerk.lengte(mystring1);
+
+        //Oefening 9:
+        System.out.println("\nOefening 9: ");
+        labwerk.evenGetallen(numbers);
+
+        //Oefening 10:
+        System.out.println("\nOefening 10: ");
+        labwerk.vindenWoord(mystring1);
+
+        //Oefening 11:
+        System.out.println("\nOefening 11: ");
+        labwerk.geenDouble(numbers);
     }
 }
